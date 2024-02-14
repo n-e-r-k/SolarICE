1391cea1ac3b7d0cb82f5ba1f9b0937f995a8bb0
nerk <nerk@vscode.pve.eridani>
2024-02-14 12:33:21 -0600

    Fix inaccurate colors in  for matching

--- switch.sh
+++ switch.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-sed -i 's/522139/81559B/g' *.xpm
-sed -i 's/B54A7B/8C86AA/g' *.xpm
-sed -i 's/DEADC6/F3F7F0/g' *.xpm
+PRIMARY='81559B' # Default: Royal purple
+SECONDARY='8C86AA' # Default: Cool gray
+CONTRAST='2E0219' # Default: Dark purple
+
+sed -i 's/B24D7A/221E22/g' *.xpm #dark
+sed -i 's/B54A7B/574C8F/g' *.xpm #primary
+sed -i 's/AFB2C3/8C86AA/g' *.xpm #light
+
+#sed -i 's/B24D7A/81559B/g' default.theme
+#sed -i 's/B54A7B/8C86AA/g' default.theme
+#sed -i 's/AFB2C3/F3F7F0/g' default.theme
+
+#sed -i 's/522139/81559B/g' ./icons/*.xpm
+#sed -i 's/B54A7B/8C86AA/g' ./icons/*.xpm
+#sed -i 's/DEADC6/2E0219/g' ./icons/*.xpm
