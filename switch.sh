c5b514f58b3a4949f9da57215266a7b8eb7a6d6f
nerk <nerk@vscode.pve.eridani>
2024-02-14 19:05:24 -0600

    Changed order of operations for sed script.

--- switch.sh
+++ switch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-LIGHT='8C86AA' # Default: Cool Gray #8C86AA
 PRIMARY='574C8F' # Default: Ultra Violet #574C8F
 SHADOW='221E22' # Default: Raisin Black #221E22
+LIGHT='8C86AA' # Default: Cool Gray #8C86AA
 
-sed -i 's/F3F7F0/8C86AA/g' *.xpm #light
 sed -i 's/8C86AA/574C8F/g' *.xpm #primary
 sed -i 's/81559B/221E22/g' *.xpm #dark
+sed -i 's/F3F7F0/8C86AA/g' *.xpm #light
 
 sed -i 's/B24D7A/574C8F/g' default.theme
 sed -i 's/B54A7B/221E22/g' default.theme
